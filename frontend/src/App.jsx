c1b1ed0859b6143a1b25139126068e69c320b250
Khairuzzaman <kzamanbm@gmail.com>
2023-06-06 04:29:01 +0800

    get to login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import LoginPage from "./components/LoginPage";
+import UserContext from "./context/user";
+import HomePage from "./components/HomePage";
+import RegistrationPage from "./components/RegistrationPage";
 
 function App() {
+  const [accessToken, setAccessToken] = useState("");
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [showLogin, setShowLogin] = useState(true);
+
   return (
-    <div>
-      <h2>GA SEI</h2>
-    </div>
+    <>
+      <UserContext.Provider
+        value={{ accessToken, setAccessToken, isAdmin, setIsAdmin }}
+      >
+        {accessToken.length > 0 && <HomePage />}
+        {accessToken.length === 0 && showLogin && (
+          <LoginPage setShowLogin={setShowLogin} />
+        )}
+        {accessToken.length === 0 && !showLogin && (
+          <RegistrationPage setShowLogin={setShowLogin} />
+        )}
+      </UserContext.Provider>
+    </>
   );
 }
 
---
+++ frontend/src/components/LoginPage.jsx
@@ -0,0 +1,75 @@
+import React, { useContext, useState } from "react";
+import { fetchData } from "../helpers/common";
+import jwt_decode from "jwt-decode";
+import UserContext from "../context/user";
+
+const LoginPage = (props) => {
+  const userCtx = useContext(UserContext);
+  const [email, setEmail] = useState("kzamanwar@gmail.com");
+  const [password, setPassword] = useState("password");
+
+  const handleLogin = async () => {
+    const { ok, data } = await fetchData("/auth/login", undefined, "POST", {
+      email,
+      password,
+    });
+
+    if (ok) {
+      userCtx.setAccessToken(data.access);
+      const decoded = jwt_decode(data.access);
+      console.log(decoded);
+      userCtx.setIsAdmin(decoded.role);
+    } else {
+      console.log(data);
+    }
+  };
+
+  return (
+    <>
+      <br />
+      <div className="row">
+        <div className="col-md-4"></div>
+        <input
+          type="text"
+          className="col-md-4"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <div className="col-md-4"></div>
+      </div>
+      <div className="row">
+        <div className="col-md-4"></div>
+        <input
+          type="password"
+          className="col-md-4"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div className="col-md-4"></div>
+      </div>
+      <div className="row">
+        <div className="col-md-4"></div>
+        <button className="col-md-4" type="submit" onClick={handleLogin}>
+          login
+        </button>
+        <div className="col-md-4"></div>
+      </div>
+      <br />
+      <div className="row">
+        <div className="col-md-4"></div>
+        <button
+          className="col-md-4"
+          type="submit"
+          onClick={() => props.setShowLogin(false)}
+        >
+          go to registion screen
+        </button>
+        <div className="col-md-4"></div>
+      </div>
+    </>
+  );
+};
+
+export default LoginPage;
